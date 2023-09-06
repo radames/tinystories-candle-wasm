d04fa6b25c35fe07db5632106bb2db4ad617ad69
Radames Ajna <radamajna@gmail.com>
2023-09-06 01:05:23 -0700

    first

---
+++ code.js
@@ -0,0 +1,246 @@
+import { select } from "https://cdn.skypack.dev/d3-selection@3";
+import { color, hcl } from "https://cdn.skypack.dev/d3-color@3";
+import {
+  piecewise,
+  interpolateRgb,
+} from "https://cdn.skypack.dev/d3-interpolate@3";
+const d3 = { select, color, hcl, piecewise, interpolateRgb };
+
+const colorPalette = d3.piecewise(d3.interpolateRgb.gamma(2.2), [
+  "#0E21A0",
+  "#4D2DB7",
+  "#9D44C0",
+  "#EC53B0",
+  "#F57B55",
+  "#FFDB5A",
+]);
+
+class WorkerPool {
+  constructor(size) {
+    this.workers = [];
+    this.size = size;
+    this.init();
+  }
+  get size() {
+    return this._size;
+  }
+  set size(size) {
+    this._size = size;
+  }
+  init() {
+    console.log("init", this.size);
+    for (let i = 0; i < this.size; i++) {
+      this.workers.push({
+        worker: new Worker("./llama2c.worker.js", { type: "module" }),
+        inUse: false,
+        id: window.crypto.randomUUID(),
+      });
+    }
+  }
+
+  getWorker() {
+    return new Promise((resolve) => {
+      const check = () => {
+        for (let worker of this.workers) {
+          if (!worker.inUse) {
+            worker.inUse = true;
+            resolve(worker.worker);
+            return;
+          }
+        }
+        setTimeout(check, 100);
+      };
+      check();
+    });
+  }
+
+  releaseWorker(worker) {
+    this.workers.find((w) => w.worker === worker).inUse = false;
+  }
+}
+
+// base url for audio examples
+const MODELS_BASE_URL =
+  "https://huggingface.co/karpathy/tinyllamas/resolve/main";
+
+// models base url
+const MODELS = {
+  stories15M: {
+    url: "stories15M.bin",
+    seq_len: 256,
+  },
+  stories42M: {
+    url: "stories42M.bin",
+    seq_len: 1024,
+  },
+  stories110M: {
+    url: "stories110M.bin",
+    seq_len: 1024,
+  },
+};
+
+const workerPool = new WorkerPool(window.navigator.hardwareConcurrency);
+async function generateSequence({
+  worker,
+  prompt,
+  weightsURL,
+  modelID,
+  maxSeqLen,
+  temp,
+  repeatPenalty,
+  contentEl,
+  controller,
+}) {
+  return new Promise((resolve, reject) => {
+    const seed = BigInt(Math.floor(Math.random() * 2 ** 64 - 1));
+    worker.postMessage({
+      weightsURL,
+      modelID,
+      tokenizerURL: "tokenizer.json",
+      prompt,
+      temp,
+      repeatPenalty,
+      seed: seed,
+      maxSeqLen,
+      command: "start",
+    });
+
+    function handleAbort() {
+      console.log("abort");
+      worker.postMessage({ command: "abort" });
+    }
+    function updateStatus(data) {
+      if (data.status === "generating") {
+        const { message, prompt, sentence, tokensSec, totalTime } = data;
+        contentEl.innerHTML = `<span class="font-semibold">${prompt}</span>
+        ${sentence.replace(/\<s\>|\<\/s\>/g, "")}`;
+      }
+    }
+    const handleMessage = (event) => {
+      const { status, error } = event.data;
+      if (status) updateStatus(event.data);
+      if (error) reject(new Error(error));
+      if (status === "complete" || status === "aborted") {
+        workerPool.releaseWorker(worker);
+        resolve(event.data);
+      }
+    };
+    controller.signal.addEventListener("abort", handleAbort);
+    worker.addEventListener("message", handleMessage);
+  });
+}
+
+async function initWorkers() {
+  const containerEl = d3.select("#container");
+  d3.select("#model").on("input", (e) => {
+    const model = MODELS[e.target.value];
+    d3.select("#max-seq").property("max", model.seq_len);
+  });
+  const containers = await Promise.all(
+    workerPool.workers.map(async (_, i) => {
+      const contentEl = d3.select(document.createElement("div"));
+
+      contentEl.on("pointerover pointerdown", (e) => {
+        d3.select(e.currentTarget).style("z-index", 100);
+      });
+      contentEl.on("pointerout pointercancel pointerleave", (e) => {
+        d3.select(e.currentTarget).style("z-index", "unset");
+      });
+
+      const bgColor = colorPalette(i / workerPool.size);
+      const fontColor = d3.hcl(bgColor).l < 50 ? "#fff" : "#000";
+      contentEl
+        .style("background-color", bgColor)
+        .style("color", fontColor)
+        .style("grid-row-start", `${i + 1}`)
+        .classed("c-block ", true);
+
+      containerEl.append(() => contentEl.node());
+      return [contentEl.node(), await workerPool.getWorker()];
+    })
+  );
+  return containers;
+}
+
+async function fetchArrayBuffer(url) {
+  const cacheName = "llama2c-candle-cache";
+  const cache = await caches.open(cacheName);
+  const cachedResponse = await cache.match(url);
+  if (cachedResponse) {
+    const data = await cachedResponse.arrayBuffer();
+    return new Uint8Array(data);
+  }
+  const res = await fetch(url, { cache: "force-cache" });
+  cache.put(url, res.clone());
+  return new Uint8Array(await res.arrayBuffer());
+}
+
+async function run(containers, controller) {
+  const getValue = (e) => e.value;
+  const prompt = document.querySelector("#prompt");
+  const maxSeqLen = document.querySelector("#max-seq");
+  const temp = document.querySelector("#temperature");
+  const repeatPenalty = document.querySelector("#repeat-penalty");
+  const modelID = document.querySelector("#model");
+
+  const weightsURL = `${MODELS_BASE_URL}/${MODELS[getValue(modelID)].url}`;
+  maxSeqLen.disabled = true;
+  temp.disabled = true;
+  repeatPenalty.disabled = true;
+  modelID.disabled = true;
+
+  // pre fetch and cache weights and tokenizer
+  await Promise.all([
+    fetchArrayBuffer(weightsURL),
+    fetchArrayBuffer(`tokenizer.json`),
+  ]);
+  await Promise.all(
+    containers.map(([container, worker]) =>
+      generateSequence({
+        worker,
+        prompt: getValue(prompt),
+        weightsURL,
+        modelID: getValue(modelID),
+        maxSeqLen: getValue(maxSeqLen),
+        temp: getValue(temp),
+        repeatPenalty: getValue(repeatPenalty),
+        contentEl: container,
+        controller,
+      })
+    )
+  );
+  maxSeqLen.disabled = false;
+  temp.disabled = false;
+  repeatPenalty.disabled = false;
+  modelID.disabled = false;
+}
+
+initWorkers().then((containers) => {
+  const runBtn = d3.select(document.querySelector("#run"));
+  let runController = new AbortController();
+  let isRunning = false;
+  d3.select("#form").on("submit", async (e) => {
+    e.preventDefault();
+
+    if (isRunning) {
+      console.log("stop");
+      stopRunning();
+    } else {
+      console.log("start");
+      startRunning();
+      await run(containers, runController);
+      stopRunning();
+    }
+  });
+
+  function startRunning() {
+    isRunning = true;
+    runBtn.text("Stop");
+  }
+  function stopRunning() {
+    runBtn.text("Run");
+    runController.abort();
+    runController = new AbortController();
+    isRunning = false;
+  }
+});
---
+++ lib/m.d.ts
@@ -0,0 +1,67 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export class Model {
+  free(): void;
+/**
+* @param {Uint8Array} weights
+* @param {Uint8Array} tokenizer
+*/
+  constructor(weights: Uint8Array, tokenizer: Uint8Array);
+/**
+* @returns {number}
+*/
+  get_seq_len(): number;
+/**
+* @param {string} prompt
+* @param {number} temp
+* @param {number} repeat_penalty
+* @param {bigint} seed
+* @returns {string}
+*/
+  init_with_prompt(prompt: string, temp: number, repeat_penalty: number, seed: bigint): string;
+/**
+* @returns {string}
+*/
+  next_token(): string;
+}
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly __wbg_model_free: (a: number) => void;
+  readonly model_new: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly model_get_seq_len: (a: number) => number;
+  readonly model_init_with_prompt: (a: number, b: number, c: number, d: number, e: number, f: number, g: number) => void;
+  readonly model_next_token: (a: number, b: number) => void;
+  readonly main: (a: number, b: number) => number;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_exn_store: (a: number) => void;
+  readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {SyncInitInput} module
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
